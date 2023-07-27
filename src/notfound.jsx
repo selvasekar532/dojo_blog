080a5317aaacdd30e304daea7f62587f249159fa
Lingesh machine <selvasekar532@gmail.com>
2023-07-27 11:45:23 +0530

    end of dojoblog

---
+++ src/notfound.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return ( 
+        <div className="not-found">
+            <h2>Sorry!</h2>
+            <h2>That page cannot be found</h2>
+            <Link to="/">Back to the homepage...</Link>
+        </div>
+     );
+}
+ 
+export default NotFound;
